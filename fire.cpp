47a324771738013cf36997f60f8d50a23b032bf4
kennyjijet <usistal@hotmail.com>
2021-01-23 22:55:39 +0700

    fix: added condition to print out fire detection.

--- fire.cpp
+++ fire.cpp
@@ -32,13 +32,15 @@ fire::fire(QString path)
         video >> originalFrame;
         cv::GaussianBlur(originalFrame, blurFrame, cv::Size(3, 3), 0); // Gaussian Blurring. In this method, instead of a box filter, a Gaussian kernel is used. It is done with the function
         cv::cvtColor(blurFrame, hsvFrame, cv::COLOR_BGR2HSV); // convert colour RGB/BGR to HSV
-        cv::inRange(hsvFrame, lower, upper, mask); //
-        cv::bitwise_and(originalFrame,hsvFrame, output, mask); //
-        no_red = cv::countNonZero(mask);
-        if (no_red > 5000) {
+        cv::inRange(hsvFrame, lower, upper, mask); // Perform basic thresholding operations using OpenCV
+        cv::bitwise_and(originalFrame,hsvFrame, output, mask); // Learn several arithmetic operations on images, like addition, subtraction, bitwise operations, and etc.
+        no_red = cv::countNonZero(mask); // returns the number of nonzero pixels in the array matrix. Zero pixels denote black color and this function can come in handy if calculating the number of black pixels or number of white pixels
+        if (no_red > 5000 && no_red < 10000) {
             std::cout << "Small Fire detected " << no_red << std::endl;
         } else if (no_red > 10000) {
             std::cout << "Big Fire detected " << no_red <<std::endl;
+        } else {
+            std::cout << "No Fire detected " << no_red <<std::endl;
         }
         channels.clear();
         cv::imshow("OriginalVideo", originalFrame);
